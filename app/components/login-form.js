60330b48d11e9d94729e8a8e795dbdcba0a2fcaf
kaisa-mill <kaisa@synap.ac>
2021-11-24 14:23:21 +0000

    decorators(action)

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,16 +1,22 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
 
 export default class LoginFormComponent extends Component {
+
+    loginAsUserWithId(val) {
+        console.log('UserId: ', val);
+    }
     /**
      * 
      * @param {Event & {target: HTMLFormElement}} evt 
      */
 
+    @action
     onLoginFormSubmit(evt){
         const { target } = evt;
         const val = target.querySelector('select').value;
         evt.preventDefault();
-        console.log("userid: ", val)
-        // debugger;
+        debugger;
+        this.loginAsUserWithId(val);
     }
 }
