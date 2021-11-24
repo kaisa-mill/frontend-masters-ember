0c682baa58717c43d6f2a1a9a2401f812c32c36e
kaisa-mill <kaisa@synap.ac>
2021-11-24 17:16:58 +0000

    integration tests for logout

--- tests/stubs/auth-service.js
+++ tests/stubs/auth-service.js
@@ -1,6 +1,7 @@
 import Service from '@ember/service';
 import { inject as service } from '@ember/service';
 import Router from '@ember/routing/router';
+import { action } from '@ember/object';
 
 export default class MockAuthService extends Service {
     currentUserId = null;
@@ -8,9 +9,15 @@ export default class MockAuthService extends Service {
      * @type {Router}
      */
     @service router;
-    
+
     loginWithUserId(userId) {
         this.currentUserId = userId;
         this.router.transitionTo('teams');
+    }
+
+    @action
+    logout() {
+        this.currentUserId = null;
+        this.router.transitionTo('login');
     }
 }
