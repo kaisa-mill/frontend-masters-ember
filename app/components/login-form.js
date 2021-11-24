97afb387fdb09aeeb4ac5ac66bd3d0a4859881df
kaisa-mill <kaisa@synap.ac>
2021-11-24 14:06:55 +0000

    handling form submit

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -7,6 +7,10 @@ export default class LoginFormComponent extends Component {
      */
 
     onLoginFormSubmit(evt){
-        debugger;
+        const { target } = evt;
+        const val = target.querySelector('select').value;
+        evt.preventDefault();
+        console.log("userid: ", val)
+        // debugger;
     }
 }
