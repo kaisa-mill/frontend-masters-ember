705548c8aedfa01f1a7e756235f42301b87181d9
kaisa-mill <kaisa@synap.ac>
2021-11-25 11:17:52 +0000

    making API calls

--- tests/integration/components/team-sidebar-test.js
+++ tests/integration/components/team-sidebar-test.js
@@ -9,8 +9,16 @@ module('Integration | Component | team-sidebar', function (hooks) {
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('myTeam', {
+      name: '',
+      channels: [
+        {
+          name: 'general',
+        }
+      ]
+    })
 
-    await render(hbs`<TeamSidebar />`);
+    await render(hbs`<TeamSidebar @team={{this.myTeam}} />`);
 
     assert.deepEqual(
       this.element.textContent
