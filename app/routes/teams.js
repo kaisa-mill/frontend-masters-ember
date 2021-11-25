5f9dc50611d53fc4b09f4a40794250c4e179da67
kaisa-mill <kaisa@synap.ac>
2021-11-25 10:24:46 +0000

    URL driven models

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -2,6 +2,27 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import AuthService from 'shlack/services/auth';
 
+const ALL_TEAMS = [
+    {
+        "id": "linkedin",
+        "name": "LinkedIn",
+        "order": 2,
+        "iconUrl": "/assets/img/linkedin.png"
+      },
+      {
+        "id": "ms",
+        "name": "Microsoft",
+        "order": 3,
+        "iconUrl": "/assets/img/microsoft.png"
+      },
+      {
+        "id": "avengers",
+        "name": "Avengers",
+        "order": 4,
+        "iconUrl": "/assets/img/avengers.jpg"
+      },
+]
+
 export default class TeamsRoute extends Route {
     /**
      * @type {AuthService}
@@ -13,5 +34,8 @@ export default class TeamsRoute extends Route {
         if (!this.auth.currentUserId) {
             this.transitionTo('login');
         }
+    }
+    async model() {
+       return ALL_TEAMS; 
     }
 }
