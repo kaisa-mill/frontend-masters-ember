2bb00ecefc31c95b3f688a5d43459b0e4aff0b9c
kaisa-mill <kaisa@synap.ac>
2021-11-25 10:49:41 +0000

    introduction to fetch

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -1,27 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import AuthService from 'shlack/services/auth';
-
-export const ALL_TEAMS = [
-    {
-        "id": "linkedin",
-        "name": "LinkedIn",
-        "order": 2,
-        "iconUrl": "/assets/img/linkedin.png"
-      },
-      {
-        "id": "ms",
-        "name": "Microsoft",
-        "order": 3,
-        "iconUrl": "/assets/img/microsoft.png"
-      },
-      {
-        "id": "avengers",
-        "name": "Avengers",
-        "order": 4,
-        "iconUrl": "/assets/img/avengers.jpg"
-      },
-]
+import fetch from 'fetch';
 
 export default class TeamsRoute extends Route {
     /**
@@ -36,6 +16,7 @@ export default class TeamsRoute extends Route {
         }
     }
     async model() {
-       return ALL_TEAMS; 
+       const response = await fetch('/api/teams');
+       return response.json(); 
     }
 }
