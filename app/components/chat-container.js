ed9b2c7d79404db77807a819da7ccf75e23cfc67
kaisa-mill <kaisa@synap.ac>
2021-11-26 14:07:32 +0000

    createMessage action

--- app/components/chat-container.js
+++ app/components/chat-container.js
@@ -2,10 +2,17 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import fetch from 'fetch';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import AuthService from 'shlack/services/auth';
 
 export default class ChatContainerComponent extends Component {
   @tracked
   messages = [];
+
+  /**
+   * @type {AuthService}
+   */
+  @service auth;
 
   @action
   async loadMessages() {
@@ -15,5 +22,25 @@ export default class ChatContainerComponent extends Component {
 
     const resp = await fetch(`/api/teams/${teamId}/channels/${id}/messages`);
     this.messages = await resp.json();
+  }
+
+  @action
+  async createMessage(body) {
+    const {
+      channel: { channelId, teamId },
+    } = this.args;
+    const userId = this.auth.currentUserId;
+    const resp = await fetch('/api/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        teamId,
+        channelId,
+        userId,
+        body
+      })
+    });
   }
 }
